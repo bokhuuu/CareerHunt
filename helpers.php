e8c4be54f38b0416aed84358cccf4484f9d852f9
bokhuuu <zurabokhua@gmail.com>
2024-08-23 11:54:54 +0400

    Refactor: Moved HTML sections into partials and added helper function
    - Updated code to include partials for header, navbar, showcase, top-banner, bottom-banner, and footer
    - Added helper function to dynamically include partial view files, with file existence check and error message if the partial is not found
    - Refactored HTML layout to use the new partials with helper function

--- helpers.php
+++ helpers.php
@@ -5,8 +5,27 @@
  * 
  * @param string $path
  * @return string
+ * 
  */
 function basePath($path = '')
 {
     return __DIR__ . '/' . $path;
+}
+
+/**
+ * Load a partial
+ * 
+ * @param string $name
+ * @return void
+ * 
+ */
+function loadPartial($name)
+{
+    $partialPath =  basePath("views/partials/{$name}.php");
+
+    if (file_exists($partialPath)) {
+        require $partialPath;
+    } else {
+        echo "Partial '{$name}' not found!";
+    }
 }
