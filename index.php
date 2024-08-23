b3c00413d4c5e68ba1d12a769b3020c79429efb3
bokhuuu <zurabokhua@gmail.com>
2024-08-23 16:53:48 +0400

    Configure routing and htaccess for XAMPP setup
    - Created and configured htaccess file to handle URL rewriting with base path /CareerHunt/
    - Defined routing logic in to map URLs to corresponding controller files and added basic content for testing
    - Adjusted HTML links to reflect new routing structure

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
 <?php
 require './helpers.php';
 
-require basePath('views/home.php');
+$routes = [
+    '/' => 'controllers/main.php',
+    '/listings' => 'controllers/listings/index.php',
+    '/listings/create' => 'controllers/listings/create.php',
+    '404' => 'controllers/error/404.php'
+];
+
+$uri = str_replace('/CareerHunt', '', $_SERVER['REQUEST_URI']);
+
+if (array_key_exists($uri, $routes)) {
+    require(basePath($routes[$uri]));
+} else {
+    require(basePath($routes['404']));
+}
